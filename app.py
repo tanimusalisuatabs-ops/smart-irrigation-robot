6bce114b9bbbede1f6f04aa59e762bf5d7fff28d
tanimusalisuatabs-ops <tanimusalisuatabs@gmail.com>
2026-09-15 02:58:54 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import os
+ffrom flask import Flask
 
 app = Flask(__name__)
-CORS(app)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run()
 
 robots = {
     1: "Soil Monitoring Robot",
